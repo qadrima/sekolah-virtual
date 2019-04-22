2a01afe9dcde5a43c6c20206edd699faf05e878a
qadrimen@gmail.com <qadrimen@gmail.com>
2019-04-22 09:01:54 +0700

    modif

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -21,16 +21,6 @@ class SubjectController extends Controller
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
-    {
-        return 'hh';
-    }
-
     public function showCreate()
     {
         if(Auth::user()->type == 1)
